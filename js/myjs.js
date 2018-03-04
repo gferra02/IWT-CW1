a61f3dea81f5d0d1a89520288cf105f839b1c89e
ginestra <ginestra@users.noreply.github.com>
2018-03-04 17:33:24 +0000

    Moving stuff around

--- js/myjs.js
+++ js/myjs.js
@@ -28,22 +28,31 @@ $(document).ready(function() {
                 //         + '</td></tr>');
                 // });
                 
+                // Start displaying the table for results
                 $('#result-list').html('<thead><tr><th>Category</th>' +
                                        '<th>Year</th><th>Name(s)</th>' +
                                        '<th>Motivation</th></tr></thead><tbody>');
-                
-                $.each(data.prizes, function(k, v) {
-                    var firstLevelArray = this;
 
-                    $.each(v.laureates, function(subk, subv) {
-                        console.log(subv)
-                        $('#result-list').append('<tr><td>' + firstLevelArray.category +
-                            '</td><td>' + firstLevelArray.year + '</td><td>' +
-                            subv.firstname + ' ' + subv.surname +
-                            '</td><td>' + subv.motivation + '</td></tr>');
-                    });
-                });
+                // Get all results if the query is empty
+                var inputs = document.getElementsByTagName('input');
+                for (var i = 0; i < inputs.length; i++) {
+                    if(inputs[i].value == ""){
+                        
+                        $.each(data.prizes, function(k, v) {
+                            var firstLevelArray = this;
 
+                            $.each(v.laureates, function(subk, subv) {
+                                console.log(subv)
+                                $('#result-list').append('<tr><td>' + firstLevelArray.category +
+                                    '</td><td>' + firstLevelArray.year + '</td><td>' +
+                                    subv.firstname + ' ' + subv.surname +
+                                    '</td><td>' + subv.motivation + '</td></tr>');
+                            });
+                        });
+                    }
+                }
+
+                // Close the table
                 $('#result-list').append('</tbody></table>');
             }
         });
