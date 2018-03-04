ceebb716915c387a14ac498920ce86b3e85b72d9
ginestra <ginestra@users.noreply.github.com>
2018-03-04 17:07:36 +0000

    Experimenting with JSPath, replacing jspath with the minified version

--- js/myjs.js
+++ js/myjs.js
@@ -4,52 +4,48 @@
 $(document).ready(function() {
 
     $('#trigger-results').click(function() {
-        var jsonFile = $('#result-list');
-        jsonFile.text('Loading...');
+        var results = $('#result-list');
+        results.text('Loading results...');
 
         $.ajax({
             type: 'GET',
             url: 'nobel.json',
             dataType: 'json',
-            // crossDomain: 'true',
-            // isLocal: 'true',
 
             // TODO: REFACTOR THIS
             success: function(data) {
-                console.log(data);
+                console.log(data); // checking the array in the console
+
                 
-                $('#result-list').html('<thead><tr><th>Category</th><th>Year</th><th>Name(s)</th><th>Motivation</th></tr></thead><tbody>');
+                $('#result-list').html('<thead><tr><th>Category</th>' +
+                                       '<th>Year</th><th>Name(s)</th>' +
+                                       '<th>Motivation</th></tr></thead><tbody>');
+
+                // Experimenting with JSPath
+                // $.each(data.prizes, function(k, v) {
+                //     $('#result-list').append('<tr><td>' +
+                //         JSPath.apply('.prizes..firstname', data)
+                //         + '</td></tr>');
+                // });
+                
+                $('#result-list').html('<thead><tr><th>Category</th>' +
+                                       '<th>Year</th><th>Name(s)</th>' +
+                                       '<th>Motivation</th></tr></thead><tbody>');
                 
                 $.each(data.prizes, function(k, v) {
                     var firstLevelArray = this;
-                    // $('#result-list').append('<tr><td>' + v.category + '</td><td>' + v.year+ '</td><td><ul class="names">');
 
-                    // this groups the category, rather than outputting the details for that record
                     $.each(v.laureates, function(subk, subv) {
                         console.log(subv)
-                        $('#result-list').append('<tr><td>' + firstLevelArray.category + '</td><td>' +
-                        firstLevelArray.year + '</td><td>' + subv.firstname + ' ' + subv.surname +
-                        '</td><td>' + subv.motivation + '</td></tr>');
+                        $('#result-list').append('<tr><td>' + firstLevelArray.category +
+                            '</td><td>' + firstLevelArray.year + '</td><td>' +
+                            subv.firstname + ' ' + subv.surname +
+                            '</td><td>' + subv.motivation + '</td></tr>');
                     });
-
-                    // $('result-list').append('</ul></td></tr>');
                 });
 
                 $('#result-list').append('</tbody></table>');
             }
         });
-    });
-
-    var result='{"rates":[{"meter":"30","rate":"0.15060","ppd":"10.000"}]}';
-    console.log(result);
-    $.each($.parseJSON(result), function (item, value) {
-        if (item == "rates") {
-            $.each(value, function (i, object) {
-                $.each(object, function (subI, subObject) {
-                    console.log(subI + "=" + subObject);
-                });
-            });
-
-        }
     });
 });
