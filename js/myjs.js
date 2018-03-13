6ea43d5f04eeceddb3d20b2ddd5e13c13e1c9cf6
gferra02 <gferra02@mail.bbk.ac.uk>
2018-03-13 11:58:15 +0000

    Fixed equal operator in html / refactor function for JSPAth

--- js/myjs.js
+++ js/myjs.js
@@ -18,23 +18,6 @@ $(document).ready(function() {
                                        '<th>Year</th><th>Firstname(s)</th>' +
                                        '<th>Surname(s)</th></tr></thead><tbody>');
 
-                /*
-                // TODO: refine, but KEEP, it works
-                // Experimenting with JSPath for filtering
-                var arrayOfResults = JSPath.apply('.prizes..firstname', data);
-
-                // console.log(arrayOfResults.length);
-                // console.log(arrayOfResults[1]);
-
-                for (i = 0; i < arrayOfResults.length; i++) {
-                    console.log('test' + [i]);
-                    $('#result-list').append(
-                        '<tr><td>' +
-                        arrayOfResults[i] +
-                        '</td></tr>');
-                }
-                */
-
                 // Get all results only if *every* input field is empty
                 // Setting empty to true since initially all fields will be blank
                 var empty = true;
@@ -48,100 +31,109 @@ $(document).ready(function() {
                     }
                 });
 
+                var path = '.prizes';
+
                 // checking the flag changes accordingly
                 // console.log(empty);
 
-                // Finally retrieving all results if all input fields are empty
                 if (empty) {
-
-                    $.each(data.prizes, function(k, v) {
-                        var firstLevelArray = this;
-
-                        $.each(v.laureates, function(subk, subv) {
-                            $('#result-list').append('<tr><td>' +
-                                firstLevelArray.category + '</td><td>' +
-                                firstLevelArray.year + '</td><td>' +
-                                subv.firstname + '</td><td>' + 
-                                subv.surname + '</td></tr>');
-                        });
-                    });
-                } else {
-                    // 1) The user should be able to enter a category value (e.g.
-                    // literature) and retrieve the corresponding prize winners.
-
-                    if (!isEmpty($('#category'))) {
-                        // Getting user input, converting it to lower case to
-                        // match lower and upper case
-                        var userCategory = $('#category').val().toLowerCase();
-
-                        filteredResults(userCategory, 'category');
-                    }
-
-                    // 2) The user should be able to enter a year value (e.g.
-                    // 1991) as well as an operator (<, = or >) and retrieve the
-                    // prize winners for the years specified.
-                    if (!isEmpty($('#year'))) {
-                        // Getting user input.
-                        // Won't check for range nor type, since already doing it
-                        // in the html form
-                        var userYear = $('#year').val();
-
-                        var userYearOperator = $('select#year-range option:selected').val();
-
-                        if (userYearOperator === '' || userYearOperator === '=') {
-                            filteredResults(userYear, 'year');
-                        } 
-                        // else if (firstLevelArray.year > userYear &&
-                        //         userYearOperator === ">") {
-                        //         $.each(v.laureates, function(subk, subv) {
-                        //             $('#result-list').append('<tr><td>' +
-                        //                 firstLevelArray.category + '</td><td>' +
-                        //                 firstLevelArray.year + '</td><td>' +
-                        //                 subv.firstname + '</td><td>' +
-                        //                 subv.surname + '</td></tr>');
-                        //         });
-                        //     }
-                    }
-
-                    if (!isEmpty($('#share'))) {
-
-                    }
-
-                    if (!isEmpty($('#surname'))) {
-
-                        // Using this to test if I can build a query string from all input fields
-                        // It works, add substitution!
-                        var testJSP = JSPath.apply('.prizes{.year < "2017" && .category == "chemistry" && ..firstname *= "otto"}', data);
-
-                        $.each(testJSP, function(k, v) {
-                            $.each(v.laureates, function(subk, subv) {
-                                $('#result-list').append('<tr><td>' +
-                                    v.category + '</td><td>' +
-                                    v.year + '</td><td>' +
-                                    subv.firstname + '</td><td>' +
-                                    subv.surname + '</td></tr>');
-                            });
-                        });
-                    }
+                    // 5) Second part: If no conditions are entered, all prizes
+                    // and winners should be returned.
+                    // Retrieving all results if all input fields are empty
+                    var jsonQuery = JSPath.apply(path, data);
+                    results(jsonQuery);
                 }
+
+                // If one or more input fields are not empty,
+                // build the query string to retrieve the relevant data.
+
+                // 1) The user should be able to enter a category value (e.g.
+                // literature) and retrieve the corresponding prize winners.
+
+                if (!isEmpty($('#category'))) {
+                    // Getting user input, converting it to lower case to
+                    // match lower and upper case
+                    var userCategory = $('#category').val().toLowerCase();
+                    path += '{.category == "' + userCategory + '" }'; 
+                }
+
+                // 2) The user should be able to enter a year value (e.g.
+                // 1991) as well as an operator (<, = or >) and retrieve the
+                // prize winners for the years specified.
+                if (!isEmpty($('#year'))) {
+                    // Getting user input.
+                    // Won't check for range nor type, since already doing it
+                    // in the html form
+                    var userYear = $('#year').val();
+
+                    var userYearOperator = $('select#year-range option:selected').val();
+
+                    if (userYearOperator == "") {
+                        userYearOperator = "==";
+                    }
+
+                    path += '{.year ' + userYearOperator + ' "' + userYear + '" }';
+                    console.log(path);
+                }
+
+                // path = '.prizes{.category == $userCat && .year == $userY }';
+
+                jsonQuery = JSPath.apply(path, data);
+
+                if (!isEmpty($('#share'))) {
+
+                }
+
+                if (!isEmpty($('#surname'))) {
+
+                    // Using this to test if I can build a query string from all input fields
+                    // It works, add substitution!
+                    // var testJSP = JSPath.apply('.prizes{.year < "2017" && .category == "chemistry" && ..surname *= "man"}', data);
+
+                    // $.each(testJSP, function(k, v) {
+                    //     $.each(v.laureates, function(subk, subv) {
+                    //         $('#result-list').append('<tr><td>' +
+                    //             v.category + '</td><td>' +
+                    //             v.year + '</td><td>' +
+                    //             subv.firstname + '</td><td>' +
+                    //             subv.surname + '</td></tr>');
+                    //     });
+                    // });
+                }
+
+                results(jsonQuery);
 
                 // Close the table
                 $('#result-list').append('</tbody>');
 
                 // Function to retrieve results
-                function filteredResults(userInput, jsonObj) {
-                    $.each(data.prizes, function(k, v) {
-                        var firstLevelArray = this;
+                // function filteredResults(userInput, jsonObj) {
+                //     $.each(data.prizes, function(k, v) {
+                //         var firstLevelArray = this;
 
-                        if (firstLevelArray[jsonObj] === userInput) {
-                            $.each(v.laureates, function(subk, subv) {
-                                $('#result-list').append('<tr><td>' +
-                                    firstLevelArray.category + '</td><td>' +
-                                    firstLevelArray.year + '</td><td>' +
-                                    subv.firstname + '</td><td>' + 
-                                    subv.surname + '</td></tr>');
-                            });
-                        }
+                //         if (firstLevelArray[jsonObj] === userInput) {
+                //             $.each(v.laureates, function(subk, subv) {
+                //                 $('#result-list').append('<tr><td>' +
+                //                     firstLevelArray.category + '</td><td>' +
+                //                     firstLevelArray.year + '</td><td>' +
+                //                     subv.firstname + '</td><td>' + 
+                //                     subv.surname + '</td></tr>');
+                //             });
+                //         }
+                //     });
+                // }
+
+                function results(jsonQuery) {
+                    var query = jsonQuery;
+
+                    $.each(query, function(k, v) {
+                        $.each(v.laureates, function(subk, subv) {
+                            $('#result-list').append('<tr><td>' +
+                                v.category + '</td><td>' +
+                                v.year + '</td><td>' +
+                                subv.firstname + '</td><td>' +
+                                subv.surname + '</td></tr>');
+                        });
                     });
                 }
             }
